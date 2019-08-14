4782a6936c4abbe2157eafb8314149c3d51495c5
Qi Yuan <yqqbeyond@gmail.com>
2019-08-14 19:42:53 +0800

    Create 1.cpp

---
+++ Leetcode/1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        bool found = false;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    res.push_back(i);
+                    res.push_back(j);
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+        return res;
+    }
+};
